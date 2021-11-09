4cc7ea40a7eddcb3edf898bedf8f0e60b1dc9bde
Josh <barlow.joshua@gmail.com>
2021-11-09 23:11:53 +0000

    Update Camera.gd

--- bug-game/Camera.gd
+++ bug-game/Camera.gd
@@ -5,7 +5,6 @@ var mothNode
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	mothNode = get_node("../ButterflyCharacter/Position3D").get_global_transform()
-	print(mothNode)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	mothNode = get_node("../ButterflyCharacter/Position3D").get_global_transform()
